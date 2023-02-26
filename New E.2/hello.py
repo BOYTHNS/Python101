484e5a2ecb29ad7237ec49c94584422c547e9111
BOYTHNS <126379507+BOYTHNS@users.noreply.github.com>
2023-02-26 15:42:30 +0700

    Test Hello news EP.3

--- New E.2/hello.py
+++ New E.2/hello.py
@@ -1,9 +1,8 @@
 print('hello world')
-
-print('hello world')
-
 print('hello Yuwadee')
 
 print('hello Github')
 friend = ['papa','mama','Bebee','jajay']
 print(friend[0])
+
+print('hello mamapapa')
